743f5edc11f501d8ba180207637ad1ffef5f8a6d
arnoldo1996 <achinaka7@gmail.com>
2021-09-05 15:10:38 +0200

    Add files via upload

---
+++ coviddash.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import altair as alt
+import pandas as pd
+
+#Chart heading
+st.title('Covid Vaccines Progress over time in South Africa')
+
+# Source file
+url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/country_data/South%20Africa.csv"
+
+df = pd.read_csv(url)
+
+# Create Altair chart
+cv19 = alt.Chart(df).mark_bar().encode(
+    x='date', y='people_vaccinated',color = 'people_fully_vaccinated',
+    tooltip=['date', 'people_vaccinated','people_fully_vaccinated']).interactive().properties(
+    width=1000,
+    height=400
+)
+
+#Display chart
+st.write(cv19)
